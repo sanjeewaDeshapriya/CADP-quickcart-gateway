cae4011b9748903b66db827d901d91e2c4764d51
Dilshan Sanjeewa Deshapriya <dilshansanjeewadeshappriya@gmail.com>
2025-05-01 13:22:08 +0530

    add config settings

---
+++ src/main/java/config/GatewayConfig.java
@@ -0,0 +1,15 @@
+package config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return  builder.routes().route("quick-cart-user-service-api",r->r.path("/user-service/**").uri("http://localhost:8082/")).build();
+    }
+
+}
