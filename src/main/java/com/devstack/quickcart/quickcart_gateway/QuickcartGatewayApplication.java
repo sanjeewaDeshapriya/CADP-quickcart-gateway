d447b9023ed9826b05d622a17e0b30f4856590db
Dilshan Sanjeewa Deshapriya <dilshansanjeewadeshappriya@gmail.com>
2025-05-01 12:14:39 +0530

    Initial commit

---
+++ src/main/java/com/devstack/quickcart/quickcart_gateway/QuickcartGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.devstack.quickcart.quickcart_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class QuickcartGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickcartGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devstack/quickcart/quickcart_gateway/QuickcartGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstack.quickcart.quickcart_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickcartGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
